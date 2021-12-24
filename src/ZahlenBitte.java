99a427a6c8642510aacb0bf4476797ed482260a7
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-24 07:29:51 +0100

    Add solution

--- src/ZahlenBitte.java
+++ src/ZahlenBitte.java
@@ -1,6 +1,14 @@
 public class ZahlenBitte {
 
     public static void main(String[] args) {
-
+        int[] numbers = new int[5];
+        numbers[0] = 1337;
+        numbers[1] = 42;
+        numbers[2] = 7;
+        numbers[3] = 23;
+        numbers[4] = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
